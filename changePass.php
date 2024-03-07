08d7677ad7e81d001d35775373f148b7b79d216b
jrami <cljrami@gmail.com>
2024-03-07 16:21:54 -0300

    lala

--- changePass.php
+++ changePass.php
@@ -11,9 +11,48 @@ $ip_cliente = $_POST['ip_cliente'];
 $usuario_cliente = $_POST['usuario_cliente'];
 $password_cliente = $_POST['password_cliente'];
 
-// Aquí puedes procesar las variables recibidas según tus necesidades
-// Por ejemplo, podrías llamar a las funciones definidas en tu script original, como ping y PowerShellCC.
+// GET IP
+function getIp(): string
+{
+  if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) { // Soporte de Cloudflare
+    $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+  } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    if (preg_match('/^(?:127|10)\.0\.0\.[12]?\d{1,2}$/', $ip)) {
+      if (isset($_SERVER['HTTP_X_REAL_IP'])) {
+        $ip = $_SERVER['HTTP_X_REAL_IP'];
+      } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+      }
+    }
+  } else {
+    $ip = '127.0.0.1';
+  }
+  if (in_array($ip, ['::1', '0.0.0.0', 'localhost'], true)) {
+    $ip = '127.0.0.1';
+  }
+  $filter = filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+  if ($filter === false) {
+    $ip = '127.0.0.1';
+  }
 
+  return $ip;
+}
+
+// Obtener la IP remota del cliente utilizando la función getIp()
+$remote_ip = getIp();
+
+// Lista de IPs permitidas (IPv4 e IPv6)
+$allowed_ips = array("186.10.5.69", "192.168.5.70", "192.168.5.1",);
+
+// Mostrar si la IP remota está permitida
+if (in_array($remote_ip, $allowed_ips)) {
+  echo "La IP: $remote_ip está permitida.\n";
+} else {
+  echo "Acceso no autorizado para la IP: $remote_ip\n";
+}
+
+//FIN GET IP
 /*-------------------------------------------------------------- 
 ## FUNCION PING
 ----------------------------------------------------------------*/
@@ -87,7 +126,7 @@ if ($pingStatus === 0) {
   // Registro de resultados de depuración PHP
   $logMessageDebug .= " - Resultado de PowerShellCC: $resultado\n";
   if (trim($resultado) === '0') {
-    $echoMessage = "La contraseña del usuario ha sido cambiada con éxito.";
+    $echoMessage = "La contraseña del usuario $usuario_cliente en la ip  $ip_cliente ha sido cambiada con éxito. La nueva constraseña es $password_cliente ";
   } elseif (trim($resultado) === '-1') {
     $echoMessage = "El usuario $usuario_cliente no existe en la máquina remota.";
   } else {
