9616e727961558f372c4dba01d9a2a56ff30f76b
jrami <cljrami@gmail.com>
2024-04-02 16:15:24 -0300

    gracias dios mio me salvaste

--- vpc_resp.php
+++ vpc_resp.php
@@ -16,7 +16,6 @@ function vpc_MetaData() // Función que define los datos del módulo almacenado 
         'DefaultSSLPort' => '5986',
     );
 }
-
 // Función para configurar las opciones del módulo
 function vpc_ConfigOptions()
 {
@@ -27,40 +26,54 @@ function vpc_ConfigOptions()
         ),
     );
 }
-
-
-
-//Obtener Datos
+//Obtener DatosMOD
 function Obtener_Datos()
 {
     $command = 'GetClientsProducts';
-    //$command = 'GetOrders'; su Uso Inhabilita dedidatedIP
     $postData = array(
-        //'id' => '2399', // ID del cliente  su Uso Inhabilita dedidatedIP
-        'clientid' => '47', // ID del cliente  
+        'clientid' => '47', // ID del cliente
     );
     $results = localAPI($command, $postData);
-    print_r($results);
+    if ($results['result'] == 'success') {
+        $productos = $results['products']['product'];
+        foreach ($productos as $producto) {
+            if ($producto['orderid'] == '2399') { // Filtrar por número de pedido 2399
+                $nombreProducto = $producto['name'];
+                //$ipDedicada = $producto['dedicatedip'];
+                $dedicatedip = $producto['dedicatedip'];
+                //$dedicatedip = $producto['dedicatedip'];
+                $numeroPedido = $producto['orderid']; // Obtener el número de pedido
+
+                // Imprime el nombre del producto, la IP dedicada y el número de pedido
+                echo "Nombre del producto: $nombreProducto\n";
+                echo "Dirección IP dedicada: $dedicatedip\n";
+                echo "Número de pedido: $numeroPedido\n";
+                // Retorna la IP dedicada para usarla fuera de la función
+                return $dedicatedip;
+            }
+        }
+    } else {
+        echo "Error al obtener la información del producto: " . $results['message'] . "\n";
+    }
 }
-Obtener_Datos();
+//Obtener_Datos();
 //Fin Obtener Datos 
-
-
 // Función para realizar la solicitud cURL
+
 function vpc_ChangePassword($params)
 {
+    //02042024
+    $dedicatedip = Obtener_Datos();
+    //02042024
     try {
-
         $postvars = array(
             'username' => $params['serverusername'],
             'passwd' => $params['serverpassword'],
-            //'domain' => $ips[0]['ip'], 
-            //'domain' => $dedicatedip, // Se utiliza la IP dedicada como dominio
-            'domain' => $params['serverip'],
+            //'domain' => $params['serverip'],
+            'domain' => $dedicatedip,  // Utiliza la IP dedicada obtenida
             'user' => $params['username'],
             'pass' => $params['password']
         );
-
         $postdata = http_build_query($postvars);
         //$url = 'https://vps06.xhost.cl/prueba_whmcs/vpc_ChangePassword_bk.php';
         $url = 'https://vps06.xhost.cl/prueba_whmcs/post.php';
@@ -70,7 +83,6 @@ function vpc_ChangePassword($params)
         if (!$curl) {
             throw new Exception('No se pudo inicializar cURL');
         }
-
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -79,42 +91,32 @@ function vpc_ChangePassword($params)
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
         $response = curl_exec($curl);
-
-
         if ($response === false) {
             throw new Exception('La acción cURL falló: ' . curl_error($curl));
         }
-
         curl_close($curl);
-
         // Guardar el resultado en el archivo de registro
         // $logMessage = date('Y-m-d H:i:s') . " - Resultado: $response\n";
         // file_put_contents('log.txt', $logMessage, FILE_APPEND);
         //echo 'La acción cURL se realizó correctamente.';
         //  echo $response;
-
         //   echo '<script>';
         //   echo 'console.log("Parámetros enviados por cURL:", ' . json_encode($params) . ');';
         //   echo '</script>';
     } catch (Exception $e) {
         echo 'Error: ' . $e->getMessage();
         return "Error";
-
         //   echo '<script>';
         //   echo 'console.log("Error en cURL:", ' . json_encode($e->getMessage()) . ');';
         //   echo '</script>';
     }
-
     return $response;
 }
 
 // Obtener los parámetros de WHMCS
-
-
 // Crear un log para resultados en log.txt
 //$logMessageInit = date('Y-m-d H:i:s') . " - Inicio de la solicitud\n";
 //file_put_contents('log.txt', $logMessageInit, FILE_APPEND);
-
 // Verificar si se obtienen datos en $params
 //var_dump($params);
 //$params = $_REQUEST;
